8d7887e42b8b93ad1dd58bfe932c75bd496636dc
Jonathan Fletcher <JFletcher94@users.noreply.github.com>
2016-12-09 14:41:00 -0500

    Add files via upload

---
+++ lab4_part2.ino
@@ -0,0 +1,44 @@
+
+
+bool waitFor(char k, unsigned int pollTime=200, unsigned long timeOut=60e6) {
+  unsigned long timeNow = micros();
+  char c = 0;
+  while (c != k) {
+    delayMicroseconds(pollTime);
+    if (micros() - timeNow > timeOut) return false;
+    if (Serial.available()) c = Serial.read();
+  }
+  return true;
+}
+
+int p = 9; //LED pin
+int b = 128; //LED brightness
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(p, OUTPUT);
+  while (!waitFor('?'));
+  Serial.print('K');
+  analogWrite(p, b);
+}
+
+void loop() {
+  while (!waitFor('g'));
+  unsigned long t0 = micros();
+  unsigned int Vs[512];
+  for (int i = 0; i < 512; i++) {
+    Vs[i] = analogRead(A0);
+  }
+  unsigned long t1 = micros();
+  Serial.println(t1 - t0);
+  for (int i = 0; i < 512; i++) {
+    Serial.println(Vs[i]);
+  }
+}
+
+
+  /*
+  while (!waitFor('?')) {
+  }
+  Serial.print('K');
+  */
---
+++ lab5_part1.py
@@ -0,0 +1,37 @@
+import time
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+    
+ser = serial.Serial('COM3')
+ser.timeout = 1
+time.sleep(2)
+
+s = ''
+while (s != 'K'):
+    time.sleep(1)
+    ser.write('?')		
+    s = ser.read(10).rstrip()
+print('connected')
+
+s = ''
+while (s != 'K'):
+    time.sleep(1)
+    ser.write('g')		
+    s = ser.read(10).rstrip()
+print('reading')
+
+X = np.arange(256)
+Y = np.zeros(256)
+for i in range(256):
+    s = ser.readline().rstrip()
+    while (s == ''):
+        s = ser.readline().rstrip()
+    Y[i] = int(s)
+plt.plot(X, Y)
+plt.xlabel('analogWrite voltage')
+plt.ylabel('Volts')
+plt.show()
+
+ser.close()
+print('disconnected')
