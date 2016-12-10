1752503e243b89a0dd748d593eb384e66796b8ff
Jonathan Fletcher <JFletcher94@users.noreply.github.com>
2016-12-10 16:38:08 -0500

    Add files via upload

---
+++ lab2/lab2_part2/lab2_part2.ino
@@ -0,0 +1,29 @@
+int p = 9; //port
+
+void setup() {
+  pinMode(p, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(p, 0);
+  delay(1000);
+  digitalWrite(p, 1);
+  delay(1000);
+  digitalWrite(p, 2);
+  delay(1000);
+  digitalWrite(p, 4);
+  delay(1000);
+  digitalWrite(p, 8);
+  delay(1000);
+  digitalWrite(p, 16);
+  delay(1000);
+  digitalWrite(p, 32);
+  delay(1000);
+  digitalWrite(p, 64);
+  delay(1000);
+  digitalWrite(p, 128);
+  delay(1000);
+  digitalWrite(p, 255);
+  delay(1000);
+}
+
---
+++ lab3/lab3.ino
@@ -0,0 +1,23 @@
+int p = 9; //LED pin
+int b = 255; //LED brightness
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(p, OUTPUT);
+  analogWrite(p, b);
+
+  unsigned long t0 = micros();
+  unsigned int Vs[512];
+  for (int i = 0; i < 512; i++) {
+    Vs[i] = analogRead(A0);
+  }
+  unsigned long t1 = micros();
+  Serial.println("time:");
+  Serial.println(t1 - t0);
+  for (int i = 0; i < 512; i++) {
+    Serial.println(Vs[i]);
+  }
+}
+
+void loop() {
+}
